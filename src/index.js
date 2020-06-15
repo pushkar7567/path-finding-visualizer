e37c1dc90c14bc958b6ba298564abb3a6510b650
Pushkar Sabharwal <pushkar7567@gmail.com>
2020-06-16 02:03:27 +0530

    adding firebase deployment

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.css';
+import {Container} from 'react-bootstrap'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Container>
+      <App />
+    </Container>
   </React.StrictMode>,
   document.getElementById('root')
 );
